9219eadb39c5649ea456ebd90eef6ce97ba7a282
Andrew Zaiko <andrewzaiko@gmail.com>
2022-12-30 21:05:22 +0300

    Создал пути 'token/', 'token/refresh/'

--- users/urls/users.py
+++ users/urls/users.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from users import views
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create/', views.UserCreateView.as_view()),
     path('<int:pk>/update/', views.UserUpdateView.as_view()),
     path('<int:pk>/delete/', views.UserDeleteView.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view())
 ]
