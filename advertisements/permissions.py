a705156ede5183366181729f7ad783e5beb0c158
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-02 19:54:31 +0300

    Создал класс 'AdvertisementUpdateDeletePermission'

--- advertisements/permissions.py
+++ advertisements/permissions.py
@@ -6,7 +6,6 @@ class AdvertisementUpdateDeletePermission(BasePermission):
     message = "Access denied for not owners or admins (moderators)"
 
     def has_permission(self, request, view):
-
         if request.user.role == UserRole.MODERATOR or \
                 request.user.role == UserRole.ADMIN:
             return True
