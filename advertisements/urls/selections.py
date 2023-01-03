45003b61622dd1137493d93c0e133ff627d79c7f
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-03 21:29:55 +0300

    Создал маршрут 'selection/create/'

--- advertisements/urls/selections.py
+++ advertisements/urls/selections.py
@@ -5,7 +5,7 @@ from advertisements import views
 urlpatterns = [
     path('', views.SelectionListView.as_view()),
     path('<int:pk>/', views.SelectionDetailView.as_view()),
-  #  path('create/', views.SelectionCreateView.as_view()),
+    path('create/', views.SelectionCreateView.as_view()),
  #   path('<int:pk>/update/', views.SelectionUpdateView.as_view()),
  #   path('<int:pk>/delete/', views.SelectionDeleteView.as_view()),
  ]
