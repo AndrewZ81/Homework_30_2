ac59133df44138a3af31bd3d547486619a239cd9
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-03 17:34:21 +0300

    Создал модель 'Selection' для таблицы с подборкой объявлений

--- advertisements/models.py
+++ advertisements/models.py
@@ -1,5 +1,5 @@
-from django.db.models import Model, CharField, \
-    PositiveIntegerField, BooleanField, ForeignKey, CASCADE, ImageField
+from django.db.models import Model, CASCADE, CharField, \
+    PositiveIntegerField, BooleanField, ForeignKey, ManyToManyField, ImageField
 
 from users.models import User
 
@@ -32,3 +32,17 @@ class Advertisement(Model):
     is_published = BooleanField()
     image = ImageField(null=True, upload_to="images")
     category = ForeignKey(Category, on_delete=CASCADE)
+
+
+class Selection(Model):
+
+    class Meta:
+        verbose_name = "Подборка объявлений"
+        verbose_name_plural = "Подборки объявлений"
+
+    def __str__(self):
+        return self.name
+
+    name = CharField(max_length=200, unique=True)
+    owner = ForeignKey(User, on_delete=CASCADE)
+    items = ManyToManyField(Advertisement)
