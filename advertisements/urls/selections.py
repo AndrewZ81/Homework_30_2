e922c5538593b630215def3491714803ae9051b1
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-03 22:32:06 +0300

    Создал маршрут 'selection/<int:pk>/update/'

--- advertisements/urls/selections.py
+++ advertisements/urls/selections.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.SelectionListView.as_view()),
     path('<int:pk>/', views.SelectionDetailView.as_view()),
     path('create/', views.SelectionCreateView.as_view()),
- #   path('<int:pk>/update/', views.SelectionUpdateView.as_view()),
+    path('<int:pk>/update/', views.SelectionUpdateView.as_view()),
  #   path('<int:pk>/delete/', views.SelectionDeleteView.as_view()),
  ]
