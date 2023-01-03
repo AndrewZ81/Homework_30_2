fc0b1049b1d62b0928fd0b8149a80a39cfafac7b
Andrew Zaiko <andrewzaiko@gmail.com>
2023-01-03 21:07:57 +0300

    Добавил маршрут 'selection/'

--- advertisements/urls/selections.py
+++ advertisements/urls/selections.py
@@ -3,7 +3,7 @@ from django.urls import path
 from advertisements import views
 
 urlpatterns = [
-  #  path('', views.SelectionListView.as_view()),
+    path('', views.SelectionListView.as_view()),
     path('<int:pk>/', views.SelectionDetailView.as_view()),
   #  path('create/', views.SelectionCreateView.as_view()),
  #   path('<int:pk>/update/', views.SelectionUpdateView.as_view()),
